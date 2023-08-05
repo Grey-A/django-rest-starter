c1aab88568950b4cb51a13a3262b05af32de5589
Bello Ango <angobello0@gmail.com>
2023-08-05 11:37:23 +0100

    installed gunicorn

--- main/production_settings.py
+++ main/production_settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
+# SECURITY WARNING: make sure to change this in your railway variables from the default one
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
